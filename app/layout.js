6d9123d5db4c862ffc7f6074151deb90366bbca8
Harsh Shah <hrshah211@gmail.com>
2024-01-17 12:06:28 -0500

    Merge Changes

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "./components/navbar";
+import { UserProvider } from "@auth0/nextjs-auth0/client";
 import App from "./app";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -12,9 +12,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <App children={children}/>
-      </body>
+      <UserProvider>
+        <body className={inter.className}>
+          <App children={children} />
+        </body>
+      </UserProvider>
     </html>
   );
 }
