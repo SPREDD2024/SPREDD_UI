25ad2360571d513027963648b553aa63fb5eb964
Harsh Shah <hrshah211@gmail.com>
2024-02-12 18:53:34 -0500

    Removed Signed Out

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,18 +1,18 @@
 "use client";
 import React, { useEffect } from "react";
 import Dashboard from "../components/dashboard";
-import { useUser } from "@auth0/nextjs-auth0/client";
-import { useRouter } from "next/navigation";
+// import { useUser } from "@auth0/nextjs-auth0/client";
+// import { useRouter } from "next/navigation";
 
 const DashboardRoute = () => {
-  const { user } = useUser();
-  const route = useRouter();
+  // const { user } = useUser();
+  // const route = useRouter();
 
-  useEffect(() => {
-    if (!user) {
-      route.push("/signedOut");
-    }
-  }, [user]);
+  // useEffect(() => {
+  //   if (!user) {
+  //     route.push("/signedOut");
+  //   }
+  // }, [user]);
 
   return <Dashboard />;
 };
